ee1dad4899342501d8f23a414c4f7832fb92654b
Biwott Kevin <Benoni@Biwotts-MacBook-Air.local>
2015-06-14 22:30:38 +0300

    Initial Commit

---
+++ remote procedure call/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  remote procedure call
+//
+//  Created by Biwott Kevin on 6/14/15.
+//  Copyright (c) 2015 Biwott Kevin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
